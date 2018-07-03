89b6d167ae75c6b8b6fbc113fddb14fea7ef25b5
ns <s_nakamura>
2018-07-03 19:11:48 +0900

    表記問題

--- src/cnn/operations.py
+++ src/cnn/operations.py
@@ -1,23 +1,24 @@
 import chainer
-from chainer import links as L
-from chainer import functions as F
+from chainer import links
+from chainer import functions as func
 from chainer import Sequential
 
 OPS = {
-    'none': lambda C, stride, affine: Zero(stride),
-    'avg_pool_3x3': lambda C, stride, affine: F.AveragePooling2D(ksize=3, stride=stride, pad=1, cover_all=False).apply,
-    'max_pool_3x3': lambda C, stride, affine: F.MaxPooling2D(ksize=3, stride=stride, pad=1, cover_all=False).apply,
-    'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
-    'sep_conv_3x3': lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
-    'sep_conv_5x5': lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
-    'sep_conv_7x7': lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
-    'dil_conv_3x3': lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
-    'dil_conv_5x5': lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-    'conv_7x1_1x7': lambda C, stride, affine: Sequential(
-        F.relu,
-        L.Convolution2D(C, C, (1, 7), stride=(1, stride), padding=(0, 3), nobias=True),
-        L.Convolution2D(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), nobias=True),
-        L.BatchNormalization(C, use_gamma=affine, use_beta=affine)
+    'none': lambda ch, stride, affine: Zero(stride),
+    'avg_pool_3x3': lambda ch, stride, affine:
+    func.AveragePooling2D(ksize=3, stride=stride, pad=1, cover_all=False).apply,
+    'max_pool_3x3': lambda ch, stride, affine: func.MaxPooling2D(ksize=3, stride=stride, pad=1, cover_all=False).apply,
+    'skip_connect': lambda ch, stride, affine: Identity() if stride == 1 else FactorizedReduce(ch, ch, affine=affine),
+    'sep_conv_3x3': lambda ch, stride, affine: SepConv(ch, ch, 3, stride, 1, affine=affine),
+    'sep_conv_5x5': lambda ch, stride, affine: SepConv(ch, ch, 5, stride, 2, affine=affine),
+    'sep_conv_7x7': lambda ch, stride, affine: SepConv(ch, ch, 7, stride, 3, affine=affine),
+    'dil_conv_3x3': lambda ch, stride, affine: DilConv(ch, ch, 3, stride, 2, 2, affine=affine),
+    'dil_conv_5x5': lambda ch, stride, affine: DilConv(ch, ch, 5, stride, 4, 2, affine=affine),
+    'conv_7x1_1x7': lambda ch, stride, affine: Sequential(
+        func.relu,
+        links.Convolution2D(ch, ch, (1, 7), stride=(1, stride), padding=(0, 3), nobias=True),
+        links.Convolution2D(ch, ch, (7, 1), stride=(stride, 1), padding=(3, 0), nobias=True),
+        links.BatchNormalization(ch, use_gamma=affine, use_beta=affine)
     ),
 }
 
@@ -30,10 +31,10 @@ class ReLUConvBN(chainer.Chain):
     def __init__(self, in_channels, out_channels, k_size, stride, padding, affine=True):
         super(ReLUConvBN, self).__init__()
         self.op = Sequential(
-            F.relu,
-            L.Convolution2D(in_channels, out_channels, k_size, stride, padding, nobias=True),
+            func.relu,
+            links.Convolution2D(in_channels, out_channels, k_size, stride, padding, nobias=True),
             # affineがFalseだとgammaとbetaはそれぞれ1と0になる
-            L.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
+            links.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
         )
 
     def __call__(self, x):
@@ -49,11 +50,11 @@ class DilConv(chainer.Chain):
     def __init__(self, in_channels, out_channels, k_size, stride, padding, dilation, affine=True):
         super(DilConv, self).__init__()
         self.op = Sequential(
-            F.relu,
+            func.relu,
             GroupedDilConv(in_channels, out_channels, k_size, stride, padding, dilation,
                            groups=in_channels),
-            L.Convolution2D(in_channels, out_channels, ksize=1, padding=0, nobias=True),
-            L.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
+            links.Convolution2D(in_channels, out_channels, ksize=1, padding=0, nobias=True),
+            links.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
         )
 
     def __call__(self, x):
@@ -72,15 +73,15 @@ class GroupedDilConv(chainer.Chain):
         grouped_out_channels = out_channels // groups
         self.groups = groups
         with self.init_scope():
-            self.c = L.DilatedConvolution2D(grouped_in_channels, grouped_out_channels,
-                                            k_size, stride, padding, dilation, nobias=True)
+            self.c = links.DilatedConvolution2D(grouped_in_channels, grouped_out_channels,
+                                                k_size, stride, padding, dilation, nobias=True)
 
     def __call__(self, x):
         _n, _c, _h, _w = x.shape
         assert _c % self.groups
-        h = F.reshape(x, (_n * self.groups, _c // self.groups, _h, _w))
+        h = func.reshape(x, (_n * self.groups, _c // self.groups, _h, _w))
         h = self.c(h)
-        return F.reshape(h, (_n, _c, h, _w))
+        return func.reshape(h, (_n, _c, h, _w))
 
 
 class SepConv(chainer.Chain):
@@ -93,14 +94,14 @@ class SepConv(chainer.Chain):
         assert out_channels % in_channels == 0
         super(SepConv, self).__init__()
         self.op = Sequential(
-            F.relu,
-            L.DepthwiseConvolution2D(in_channels, 1, k_size, stride, padding, nobias=True),
-            L.Convolution2D(in_channels, in_channels, ksize=1, pad=0, nobias=True),
-            L.BatchNormalization(in_channels, use_gamma=affine, use_beta=affine),
-            F.relu,
-            L.DepthwiseConvolution2D(in_channels, 1, k_size, stride=1, pad=padding, nobias=True),
-            L.Convolution2D(in_channels, out_channels, ksize=1, pad=0, nobias=True),
-            L.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
+            func.relu,
+            links.DepthwiseConvolution2D(in_channels, 1, k_size, stride, padding, nobias=True),
+            links.Convolution2D(in_channels, in_channels, ksize=1, pad=0, nobias=True),
+            links.BatchNormalization(in_channels, use_gamma=affine, use_beta=affine),
+            func.relu,
+            links.DepthwiseConvolution2D(in_channels, 1, k_size, stride=1, pad=padding, nobias=True),
+            links.Convolution2D(in_channels, out_channels, ksize=1, pad=0, nobias=True),
+            links.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine),
         )
 
     def __call__(self, x):
@@ -142,13 +143,13 @@ class FactorizedReduce(chainer.Chain):
         super(FactorizedReduce, self).__init__()
         assert out_channels % 2 == 0
         with self.init_scope():
-            self.conv_1 = L.Convolution2D(in_channels, out_channels // 2, ksize=1, stride=2, pad=0, nobias=True)
-            self.conv_2 = L.Convolution2D(in_channels, out_channels // 2, ksize=1, stride=2, pad=0, nobias=True)
-            self.bn = L.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine)
+            self.conv_1 = links.Convolution2D(in_channels, out_channels // 2, ksize=1, stride=2, pad=0, nobias=True)
+            self.conv_2 = links.Convolution2D(in_channels, out_channels // 2, ksize=1, stride=2, pad=0, nobias=True)
+            self.bn = links.BatchNormalization(out_channels, use_gamma=affine, use_beta=affine)
 
     def __call__(self, x):
-        h = F.relu(x)
+        h = func.relu(x)
         h1 = self.conv_1(h)
         h2 = self.conv_2(h[:, :, 1:, 1:])
-        out = F.concat((h1, h2), axis=1)
+        out = func.concat((h1, h2), axis=1)
         return self.bn(out)
